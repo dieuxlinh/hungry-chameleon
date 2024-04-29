159ac493b221187ff3ab55485eedad069de0bf49
Linh Dinh <ld105@wellesley.edu>
2024-04-29 16:03:50 -0400

    Updated main function

--- hungry_chameleon/__main__.py
+++ hungry_chameleon/__main__.py
@@ -7,7 +7,7 @@ from game import GameController, GameModel, GameView
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((800, 600), pygame.DOUBLEBUF)
+    screen = pygame.display.set_mode((860, 600), pygame.DOUBLEBUF)
     model = GameModel(screen)
     view = GameView(screen)
     controller = GameController(model, view)
